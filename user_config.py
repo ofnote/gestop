d088fd68aefeb47bbdfa2f50493c0465745aa75d
Sriram <sriramsk1999@gmail.com>
2020-07-21 11:10:25 +0530

    Restructured user config

--- user_config.py
+++ user_config.py
@@ -3,90 +3,90 @@ The definition of the functions which will be executed when the
 corresponding gesture is detected.
 '''
 
+from pynput import mouse, keyboard
 from pynput.keyboard import Key, KeyCode
-import pyautogui
+from pynput.mouse import Button
 
+class UserConfig:
+    ''' Class for user configuration.'''
+    def __init__(self):
+        self.mouse = mouse.Controller()
+        self.keyboard = keyboard.Controller()
 
-def no_func(none):
-    ''' Dummy function. '''
-    return none
+    def no_func(self, S):
+        ''' Dummy function. '''
+        return S
 
-def take_screenshot(keyboard):
-    ''' Takes a screenshot. '''
-    keyboard.press(Key.print_screen)
-    keyboard.release(Key.print_screen)
-    return keyboard
+    def take_screenshot(self, S):
+        ''' Takes a screenshot. '''
+        self.keyboard.press(Key.print_screen)
+        self.keyboard.release(Key.print_screen)
+        return S
 
-def mode_switch(S):
-    ''' Switches modes. '''
-    S.modes = S.modes[-1:] + S.modes[:-1]
-    return S
+    def inc_volume(self, S):
+        ''' Increase system volume. '''
+        self.keyboard.press(KeyCode.from_vk(269025043))
+        self.keyboard.release(KeyCode.from_vk(269025043))
+        return S
 
-def inc_volume(keyboard):
-    ''' Increase system volume. '''
-    keyboard.press(KeyCode.from_vk(269025043))
-    keyboard.release(KeyCode.from_vk(269025043))
-    return keyboard
+    def dec_volume(self, S):
+        ''' Decrease system volume. '''
+        self.keyboard.press(KeyCode.from_vk(269025041))
+        self.keyboard.release(KeyCode.from_vk(269025041))
+        return S
 
-def dec_volume(keyboard):
-    ''' Decrease system volume. '''
-    keyboard.press(KeyCode.from_vk(269025041))
-    keyboard.release(KeyCode.from_vk(269025041))
-    return keyboard
+    def inc_brightness(self, S):
+        ''' Increase system volume. '''
+        self.keyboard.press(KeyCode.from_vk(269025026))
+        self.keyboard.release(KeyCode.from_vk(269025026))
+        return S
 
-def inc_brightness(keyboard):
-    ''' Increase system volume. '''
-    keyboard.press(KeyCode.from_vk(269025026))
-    keyboard.release(KeyCode.from_vk(269025026))
-    return keyboard
+    def dec_brightness(self, S):
+        ''' Decrease system volume. '''
+        self.keyboard.press(KeyCode.from_vk(269025027))
+        self.keyboard.release(KeyCode.from_vk(269025027))
+        return S
 
-def dec_brightness(keyboard):
-    ''' Decrease system volume. '''
-    keyboard.press(KeyCode.from_vk(269025027))
-    keyboard.release(KeyCode.from_vk(269025027))
-    return keyboard
+    def circle_detected(self, S):
+        ''' Trial function. '''
+        print("Circle has been detected.")
+        return S
 
-def circle_detected(none):
-    ''' Trial function. '''
-    print("Circle has been detected.")
-    return none
+    def reset_mouse(self, S):
+        ''' Resets the mouse flags. '''
+        self.mouse.release(Button.left)
+        S.mouse_flags['mousedown'] = False
+        S.mouse_flags['scroll'] = False
+        return S
 
-def reset_mouse(S):
-    ''' Resets the mouse flags. '''
-    pyautogui.mouseUp()
-    S.mouse_flags['mousedown'] = False
-    S.mouse_flags['scroll'] = False
-    return S
+    def disable_mouse_track(self, S):
+        ''' Disables tracking of mouse. '''
+        S.mouse_track = not S.mouse_track
+        return S
 
-def disable_mouse_track(S):
-    ''' Disables tracking of mouse. '''
-    S.mouse_track = not S.mouse_track
-    return S
+    def left_mouse_down(self, S):
+        ''' The left mouse button is clicked and held. '''
+        self.mouse.press(Button.left)
+        S.mouse_flags['mousedown'] = True
+        S.mouse_flags['scroll'] = False
+        return S
 
-def left_mouse_down(S):
-    ''' The left mouse button is clicked and held. '''
-    pyautogui.mouseDown()
-    S.mouse_flags['mousedown'] = True
-    S.mouse_flags['scroll'] = False
-    return S
+    def double_click(self, S):
+        ''' Left mouse button double click. '''
+        self.mouse.release(Button.left)
+        S.mouse_flags['mousedown'] = False
+        self.mouse.click(Button.left, 2)
+        return S
 
-def double_click(S):
-    ''' Left mouse button double click. '''
-    pyautogui.mouseUp()
-    S.mouse_flags['mousedown'] = False
-    pyautogui.doubleClick()
-    return S
+    def right_mouse_click(self,S):
+        ''' Right mouse button click. '''
+        S.mouse_flags['mousedown'] = False
+        self.mouse.click(Button.right)
+        return S
 
-def right_mouse_click(S):
-    ''' Right mouse button click. '''
-    pyautogui.mouseUp()
-    S.mouse_flags['mousedown'] = False
-    pyautogui.rightClick()
-    return S
-
-def scroll(S):
-    ''' Locks the mouse and allows scrolling. '''
-    pyautogui.mouseUp()
-    S.mouse_flags['mousedown'] = False
-    S.mouse_flags['scroll'] = True
-    return S
+    def scroll(self, S):
+        ''' Locks the mouse and allows scrolling. '''
+        self.mouse.release(Button.left)
+        S.mouse_flags['mousedown'] = False
+        S.mouse_flags['scroll'] = True
+        return S
